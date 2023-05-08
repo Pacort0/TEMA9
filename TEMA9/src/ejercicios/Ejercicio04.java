92b076bedfcf5dd5b21df5bf2fea7a200f2bb3eb
Admin <Admin@10.100.232.198>
2023-05-08 20:58:27 +0200

    ejercicios 4 y 5

--- TEMA9/src/ejercicios/Ejercicio04.java
+++ TEMA9/src/ejercicios/Ejercicio04.java
@@ -10,39 +10,47 @@ public class Ejercicio04 {
 		int numeros = 5;
 		int estrellas = 2;
 
-		TreeMap<Integer, Integer> arbolDinero = new TreeMap<>();
+		TreeMap<Integer, Integer> boleto = new TreeMap<>();
+		TreeMap<Integer, Integer> estrelas = new TreeMap<>();
 
 		Scanner sc = new Scanner(System.in);
 
 		for (int i = 0; i < numeros; i++) {
 			do {
-				System.out.println("Introduzca el " + (i + 1) + " número premiado: ");
+				System.out.println("Introduzca el " + (i + 1) + "º número premiado: ");
 				numero = sc.nextInt();
 			} while (numero < 0 || numero > 50);
-			
-			compruebaRepetido(arbolDinero, numero);
+
+			compruebaRepetido(boleto, estrelas, numero);
 		}
-		
-		for(int i = 0; i < estrellas; i++) {
+
+		for (int i = 0; i < estrellas; i++) {
 			do {
-				System.out.println("Introduzca el valor de la " + (i+1) + " estrella: ");
+				System.out.println("Introduzca el valor de la " + (i + 1) + "ª estrella: ");
 				numero = sc.nextInt();
-			}while(numero<=0 || numero>12);
-			
-			compruebaRepetido(arbolDinero, numero);
+			} while (numero <= 0 || numero > 12);
+
+			compruebaRepetido(estrelas, boleto, numero);
 		}
-		
-		System.out.println(arbolDinero);
-		
+
+		System.out.println("Boleto: " + boleto);
+		System.out.println("Estrellas: " + estrelas);
+
 		sc.close();
 	}
-	
-	public static void compruebaRepetido(TreeMap<Integer, Integer> boleto, int numero) {
-		if(!boleto.containsKey(numero)) {
-			boleto.put(numero, 1);
-		}
-		else {
-			boleto.replace(numero, boleto.get(numero), boleto.get(numero) + 1);
+
+	public static void compruebaRepetido(TreeMap<Integer, Integer> boleto1, TreeMap<Integer, Integer> boleto2,
+			int numero) {
+
+		if (!boleto1.containsKey(numero)) { //Si la llave no se encuentra en la primera colección
+			if (boleto2.containsKey(numero)) { //Pero sí se halla en la segunda colección
+				boleto1.put(numero, boleto2.get(numero) + 1); //Aumentamos en ambas colecciones el valor en 1
+				boleto2.replace(numero, boleto2.get(numero) + 1);
+			} else { //Si no está en ninguna colección, iniciamos su valor a 1
+				boleto1.put(numero, 1);
+			}
+		} else { //Si se encuentra en la primera coleccion, se aumenta en 1 su valor
+			boleto1.replace(numero, boleto1.get(numero) + 1);
 		}
 	}
 
---
+++ TEMA9/src/ejercicios/Ejercicio05.java
@@ -0,0 +1,87 @@
+package ejercicios;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Ejercicio05 {
+
+	public static void main(String[] args) {
+		int opcion;
+		String nombreProd;
+		double precioProd;
+
+		Scanner sc = new Scanner(System.in);
+
+		HashMap<String, Double> productos = new HashMap<>();
+
+		do {
+			menu();
+			opcion = sc.nextInt();
+			
+			sc.nextLine();
+
+			switch (opcion) {
+			case 1:
+				System.out.println("Introduzca el nombre del producto que desea añadir: ");
+				nombreProd = sc.nextLine();
+				System.out.println("Introduzca el precio del producto que desea añadir");
+				precioProd = sc.nextDouble();
+				
+				sc.nextLine();
+				
+				agregarProd(nombreProd, precioProd, productos);
+				break;
+			case 2:
+				System.out.println("Introduzca el nombre del producto que desea eliminar: ");
+				nombreProd = sc.nextLine();
+				if (eliminarProd(nombreProd, productos) == 0) {
+					System.err.println("Ese producto no existe");
+				}
+				break;
+			case 3:
+				for (int i = 0; i < productos.size(); i++) {
+					System.out.println(productos);
+				}
+				break;
+			case 4:
+				System.out.println("Hasta luego.");
+				break;
+			default:
+				System.err.println("Esa opción no está contemplada");
+			}
+		} while (opcion != 0);
+		
+		sc.close();
+
+	}
+
+	public static void menu() {
+		System.out.println("Introduzca una opción: " + "\n1. Añadir producto." + "\n2. Eliminar producto."
+				+ "\n3. Listar existencias." + "\n4. Salir");
+	}
+
+	public static int agregarProd(String nombre, double precio, HashMap<String, Double> productos) {
+		int existe = 0;
+
+		if (productos.containsKey(nombre)) {
+			existe = 1;
+		} else {
+			productos.put(nombre, precio);
+		}
+
+		return existe;
+	}
+
+	public static int eliminarProd(String nombre, HashMap<String, Double> productos) {
+		int existe = 1;
+
+		if (productos.containsKey(nombre)) {
+			productos.remove(nombre);
+		} else {
+			existe = 0;
+		}
+
+		return existe;
+	}
+
+}
