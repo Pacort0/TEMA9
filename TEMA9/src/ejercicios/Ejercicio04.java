88993529e8fb02b93bf5affe696d5cba79cb476d
frodriguez <frodriguez@106-24.info2>
2023-05-15 10:02:57 +0200

    ejercicio 4 bien por fin

--- TEMA9/src/ejercicios/Ejercicio04.java
+++ TEMA9/src/ejercicios/Ejercicio04.java
@@ -22,20 +22,16 @@ public class Ejercicio04 {
 					System.out.println("Introduzca el " + (i + 1) + "º número premiado: ");
 					numero = sc.nextInt();
 				} while (numero < 0 || numero > 50);
-				
-				if(!boleto.containsKey(numero)) {
+
+				if(!boleto.containsKey(numero) && !estrelas.containsKey(numero)) {
 					boleto.put(numero, 1);
-				} else {
-					boleto.replace(numero, boleto.get(numero)+1);
+				} else if (!boleto.containsKey(numero) && estrelas.containsKey(numero)){
+					boleto.put(numero, estrelas.get(numero)+1);
+					estrelas.replace(numero, estrelas.get(numero) + 1);
 				}
-				
-				if(estrelas.containsKey(numero) && boleto.containsKey(numero)) {
-					estrelas.replace(numero, estrelas.get(numero)+1);
-					boleto.replace(numero, boleto.get(numero)+1);
-				} else if (estrelas.containsKey(numero) && !boleto.containsKey(numero)){
-					estrelas.replace(numero, estrelas.get(numero)+1);
-					boleto.put(numero, 1);
-				} 
+				else {
+					boleto.replace(numero, boleto.get(numero) + 1);
+				}
 			}
 
 			for (int i = 0; i < estrellas; i++) {
@@ -43,6 +39,16 @@ public class Ejercicio04 {
 					System.out.println("Introduzca el valor de la " + (i + 1) + "ª estrella: ");
 					numero = sc.nextInt();
 				} while (numero <= 0 || numero > 12);
+
+				if(!estrelas.containsKey(numero) && !boleto.containsKey(numero)) {
+					estrelas.put(numero, 1);
+				} else if (!estrelas.containsKey(numero) && boleto.containsKey(numero)){
+					estrelas.put(numero, boleto.get(numero)+1);
+					boleto.replace(numero, boleto.get(numero) + 1);
+				}
+				else {
+					estrelas.replace(numero, estrelas.get(numero) + 1);
+				}
 			}
 
 			System.out.println("Boleto: " + boleto);
@@ -52,21 +58,6 @@ public class Ejercicio04 {
 			opcion = sc.next().charAt(0);
 		} while (opcion != 'n');
 		sc.close();
-	}
-
-	public static void compruebaRepetido(TreeMap<Integer, Integer> boleto1, TreeMap<Integer, Integer> boleto2,
-			int numero) {
-
-		if (!boleto1.containsKey(numero)) { // Si la llave no se encuentra en la primera colección
-			if (boleto2.containsKey(numero)) { // Pero sí se halla en la segunda colección
-				boleto1.put(numero, boleto2.get(numero) + 1); // Aumentamos en ambas colecciones el valor en 1
-				boleto2.replace(numero, boleto2.get(numero) + 1);
-			} else { // Si no está en ninguna colección, iniciamos su valor a 1
-				boleto1.put(numero, 1);
-			}
-		} else { // Si se encuentra en la primera coleccion, se aumenta en 1 su valor
-			boleto1.replace(numero, boleto1.get(numero) + 1);
-		}
 	}
 
 }
